d3e307844f6f57c917077e7ea7566c5e9a5f5369
Handy Dorceus <dorceus.handy@gmail.com>
2024-07-23 18:20:19 -0400

    added script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var parentIframe = window.frameElement;
+    if (parentIframe && parentIframe.classList.contains('styles_IFrame__lQG4e')) {
+        parentIframe.style.borderRadius = '15px';
+        parentIframe.style.height = '1000px';
+        parentIframe.style.width = '100%';
+        // Additional styles if needed
+    }
+});
